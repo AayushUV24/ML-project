3cb518fb29d680f8fbb7d770f30452c0164dcc35
AayushUV24 <aayush3592@gmail.com>
2025-07-10 21:06:45 +0530

    Model training

--- src/components/data_ingetion.py
+++ src/components/data_ingetion.py
@@ -10,6 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import Datatransformation
 from src.components.data_transformation import DatatransformationConfig
 
+from src.components.modle_trainer import  modeltrainerconfig,Modeltrainer
+
 @dataclass
 class DataingetionConfig:
     train_data_path: str=os.path.join('artifacts','train.csv')
@@ -50,4 +52,7 @@ if __name__=="__main__":
     train_data,test_data = obj.initiate_data_ingetion() 
 
     data_transformation = Datatransformation()
-    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)       
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = Modeltrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
