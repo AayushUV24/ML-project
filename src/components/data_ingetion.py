e1a1925f36e2e43482b2cfe6eb9b494447bd85a6
AayushUV24 <aayush3592@gmail.com>
2025-07-10 18:22:53 +0530

    Data transformation done

--- src/components/data_ingetion.py
+++ src/components/data_ingetion.py
@@ -5,7 +5,10 @@ from src.logger import logging
 import pandas as pd 
 
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
+from dataclasses import dataclass 
+
+from src.components.data_transformation import Datatransformation
+from src.components.data_transformation import DatatransformationConfig
 
 @dataclass
 class DataingetionConfig:
@@ -20,10 +23,10 @@ class DataIngetion:
     def initiate_data_ingetion(self):
         logging.info("Entered the data ingetion method or component")
         try:
-            df = pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv('notebook/data/stud.csv')
             logging.info("Read the dataset as dataframe")
 
-            os.makedirs(os.path.dirname(self.ingetion_config.train_data_path))
+            os.makedirs(os.path.dirname(self.ingetion_config.raw_data_path),exist_ok=True)
 
             df.to_csv(self.ingetion_config.raw_data_path,index=False,header = True)
 
@@ -31,7 +34,6 @@ class DataIngetion:
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingetion_config.train_data_path,index =False,header =True)
-
             test_set.to_csv(self.ingetion_config.test_data_path,index =False,header =True)
             
             logging.info("Ingetion of data is completed")
@@ -45,4 +47,7 @@ class DataIngetion:
         
 if __name__=="__main__":
     obj = DataIngetion()
-    obj.initiate_data_ingetion()        
+    train_data,test_data = obj.initiate_data_ingetion() 
+
+    data_transformation = Datatransformation()
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)       
